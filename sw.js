0a932fe78c8c52148d6491f893f1bff77d05218a
CanMete0 <126554484+CanMete0@users.noreply.github.com>
2026-08-11 10:50:18 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="snapfit-v7";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg","./sw.js"])))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./index.html")))));
